9bdcb964ed658b84add298ea8044b66ec1f2f56c
shallow90125 <shallow90125@gmail.com>
2023-07-10 06:48:29 +0900

    oukyushochi

--- api/index.py
+++ api/index.py
@@ -2,8 +2,9 @@ from flask import Flask, request
 import pickle
 import pandas as pd
 # import numpy as np
+import random
 
-modelDiabetes = pickle.load(open("./diabetes.sav", "rb"))
+# modelDiabetes = pickle.load(open("./diabetes.sav", "rb"))
 # modelStroke = pickle.load(open("./stroke.sav", "rb"))
 
 app = Flask(__name__)
@@ -16,38 +17,40 @@ def home():
 
 @app.route("/diabetes", methods=['GET'])
 def diabetes():
-    result = modelDiabetes.predict(pd.DataFrame({
-        "Age": [request.args.get("age")],
-        "HighChol": [request.args.get("highChol")],
-        "BMI": [request.args.get("bmi")],
-        "PhysActivity": [request.args.get("physActivity")],
-        "PhysHlth": [request.args.get("physHlth")],
-        "HighBP": [request.args.get("highBp")],
-    }))
-    return {"isSick": result[0]}, 200
+    # result = modelDiabetes.predict(pd.DataFrame({
+    #     "Age": [request.args.get("age")],
+    #     "HighChol": [request.args.get("highChol")],
+    #     "BMI": [request.args.get("bmi")],
+    #     "PhysActivity": [request.args.get("physActivity")],
+    #     "PhysHlth": [request.args.get("physHlth")],
+    #     "HighBP": [request.args.get("highBp")],
+    # }))
+    # return {"isSick": result[0]}, 200
+    return {"isSick": random.randint(0, 1)}, 200
 
 
-# @app.route("/hypertension", methods=['GET'])
-# def hypertension():
-#     return "1", 200
+@app.route("/hypertension", methods=['GET'])
+def hypertension():
+    return {"isSick": random.randint(0, 1)}, 200
 
 
-# @app.route("/stroke", methods=['GET'])
-# def stroke():
-#     result = modelStroke.predict(np.array([[
-#         request.args.get("age"),
-#         request.args.get("sex"),
-#         request.args.get("hypertension"),
-#         request.args.get("heartDisease"),
-#         request.args.get("everMarried"),
-#         request.args.get("workType"),
-#         request.args.get("residenceType"),
-#         request.args.get("avgGlucoseLevel"),
-#         request.args.get("bmi"),
-#         request.args.get("smokingStatus"),
-#     ]]))
-#     print(result)
-#     return {"isSick": result[0]}, 200
+@app.route("/stroke", methods=['GET'])
+def stroke():
+    #     result = modelStroke.predict(np.array([[
+    #         request.args.get("age"),
+    #         request.args.get("sex"),
+    #         request.args.get("hypertension"),
+    #         request.args.get("heartDisease"),
+    #         request.args.get("everMarried"),
+    #         request.args.get("workType"),
+    #         request.args.get("residenceType"),
+    #         request.args.get("avgGlucoseLevel"),
+    #         request.args.get("bmi"),
+    #         request.args.get("smokingStatus"),
+    #     ]]))
+    #     print(result)
+    #     return {"isSick": result[0]}, 200
+    return {"isSick": random.randint(0, 1)}, 200
 
 
 app.run()
